c5d9da191aa7759841b196f6440823541a03c8dd
Martin <crh944015688@gmail.com>
2017-06-28 00:21:40 +0800

    😎

--- DropApp/ViewController.m
+++ DropApp/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "MobileCoreServices/MobileCoreServices.h"
 
-@interface ViewController ()
+@interface ViewController () <UIDropInteractionDelegate>
+
+@property (nonatomic, strong) UIImageView *imageView;
 
 @end
 
@@ -16,14 +19,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // imageView
+    UIDropInteraction *dropInteraction = [[UIDropInteraction alloc] initWithDelegate:self];
+    
+    
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake((self.view.bounds.size.width / 2 - 150) / 2 , (self.view.bounds.size.height - 150) / 2, 150, 150)];
+    self.imageView.backgroundColor = [UIColor grayColor];
+    [self.imageView addInteraction:dropInteraction];
+    self.imageView.userInteractionEnabled = YES;
+    [self.view addSubview:self.imageView];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)dropInteraction:(UIDropInteraction *)interaction canHandleSession:(id<UIDropSession>)session {
+    NSLog(@"%@",session);
+    BOOL canHandle = [session hasItemsConformingToTypeIdentifiers:@[(__bridge NSString *)kUTTypeImage]] && session.items.count > 0;
+    return canHandle;
 }
 
+- (void)dropInteraction:(UIDropInteraction *)interaction sessionDidEnter:(id<UIDropSession>)session {
+    
+}
+
+- (UIDropProposal *)dropInteraction:(UIDropInteraction *)interaction sessionDidUpdate:(id<UIDropSession>)session {
+    CGPoint point = [session locationInView:self.view];
+    
+    UIDropOperation dropOperation;
+    
+    if (point.x > self.imageView.frame.origin.x && point.x < self.imageView.frame.origin.x + self.imageView.frame.size.width && point.y > self.imageView.frame.origin.y && point.y < self.imageView.frame.origin.y + self.imageView.frame.size.height) {
+        
+        // 其他app传过来的数据localDragSession为nil
+        dropOperation = session.localDragSession == nil ? UIDropOperationCopy : UIDropOperationMove;
+    }else {
+        dropOperation = UIDropOperationCancel;
+    }
+    
+    return [[UIDropProposal alloc] initWithDropOperation:dropOperation];
+}
+
+- (void)dropInteraction:(UIDropInteraction *)interaction performDrop:(id<UIDropSession>)session {
+    [session loadObjectsOfClass:[UIImage class] completion:^(NSArray<__kindof id<NSItemProviderReading>> * _Nonnull objects) {
+        UIImage *image = (UIImage *)[objects firstObject];
+        self.imageView.image = image;
+    }];
+}
 
 @end
